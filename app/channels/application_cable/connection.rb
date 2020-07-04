cd160eef9a7cba9acca80c30224501720225d1a6
Togo <a.kirigo.t0630@gmail.com>
2020-07-04 10:18:29 +0000

    [失敗]actioncable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    pravate
+      def find_varified_user
+        session_key = cookies.encrypted[Rails.application.config.session_options[:key]]
+        verified_id = session_key['warden.user.user.key'][0][0]
+        varified_user = User.find_by(id: verified_id)
+        return reject_unauthorized_connection unless varified_user
+        varified_user
+      end
   end
 end
---
+++ app/channels/room_channel.rb
@@ -0,0 +1,16 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak
+    message = Message.create!(content: data['message'], user_id :current_user.id, room_id: params['room_id'])
+    render_message = ApplicationController.renderer.render(partial: 'messages/message', locals: {message: message})
+    ActionCable.server.broadcast 'room_channel', message: render_message, room_id: params['room_id']
+  end
+end
+
